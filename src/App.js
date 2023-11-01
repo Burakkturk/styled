e8898ad287ab209923df64780d2c2582727a8c30
Burak <burakturk@gmail.com>
2023-11-02 00:26:44 +0300

    styled

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import { Button, DarkButton } from "./components/Button.styled";
 const App = () => {
   return (
     <>
-      <h1>Styled Components</h1>
+    <Flex>
+    <h1>Styled Components</h1>
       <Button>Tikla</Button>
       <DarkButton>Send</DarkButton>
+    </Flex>
+      
     </>
   );
 };
