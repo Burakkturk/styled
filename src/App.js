d8c6beb595ed07329580f93cec18110d500d64f7
Burak <burakturk@gmail.com>
2023-11-01 23:39:40 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Button } from "./components/Button.styled";
+
+const App = () => {
+  return (
+    <>
+      <h1>Styled Components</h1>
+      <Button>Tikla</Button>
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Button.styled.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  background-color: hotpink;
+  color: white;
+  border: 2px solid hotpink;
+  font-size: 1.3 rem;
+  padding: 0.7rem 3rem;
+  border-radius: 20px;
+  font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS", sans-serif;
+  &:hover{
+    cursor: pointer;
+    transform: scale(0.96);
+  }
+`;
